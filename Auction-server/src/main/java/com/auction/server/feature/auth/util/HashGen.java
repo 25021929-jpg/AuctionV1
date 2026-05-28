be05488c8f41267e60fdc07d5e42a7c7ed5401ca
25021929 <25021929@vnu.edu.vn>
2026-05-28 21:32:32 +0700

    Phát hoàn thành reository, service, controller

--- Auction-server/src/main/java/com/auction/server/feature/auth/util/HashGen.java
+++
@@ -1,7 +0,0 @@
-package com.auction.server.feature.auth.util;
-
-public class HashGen {
-    public static void main(String[] args) {
-        System.out.println(PasswordUtil.hashPassword("123456789"));
-    }
-}
