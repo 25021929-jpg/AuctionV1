407842776db86f473266f618205eaa9115dcd3b5
25021929 <25021929@vnu.edu.vn>
2026-06-01 03:16:27 +0700

    cuối cùng

---
+++ Auction-client/src/main/java/com/auction/client/ClientLauncher.java
@@ -0,0 +1,11 @@
+package com.auction.client;
+
+public final class ClientLauncher {
+
+    private ClientLauncher() {
+    }
+
+    public static void main(String[] args) {
+        MainClient.main(args);
+    }
+}
